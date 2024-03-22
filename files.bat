001ab9b285025805829925e34377d0cff0e33e1b
FlushA <kb.bytes2020@gmail.com>
2024-03-22 15:40:14 +0330

    update generator

--- files.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git diff --name-status | findstr "^M" > modified.txt
-git diff --name-status | findstr "^D" > deleted.txt
-git diff --name-status | findstr "^A" > added.txt
-echo "Modified files saved to modified.txt"
-echo "Deleted files saved to deleted.txt"
-echo "Added files saved to added.txt"
-pause
